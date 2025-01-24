45fab38c46a2d8861ca0f2f2c7c845a0b869eefa
apcagcatap <agcatap@student.apc.edu.ph>
2025-01-24 08:36:36 +0800

    f

---
+++ vue-project/src/components/FoodItem2.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-on:click="countClicks">
+    <h2>{{ name }}</h2>  
+    <p>{{ message }}</p>
+    <p id="red">You have clicked me {{ clicks }} times.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Apples',
+      message: 'I like apples',
+      clicks: 0
+    }
+  },
+  methods: {
+    countClicks() {
+      this.clicks++;
+    }
+  }
+}
+</script>
+
+<style>
+  #red {
+    font-weight: bold ;
+    color: rgb(144, 12, 12);
+  }
+    #app > div {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: lightgreen;
+  }
+</style>
