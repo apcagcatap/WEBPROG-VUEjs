7a251dcd5e7b668177d9eb032b35b3c54d5e6a7d
apcagcatap <agcatap@student.apc.edu.ph>
2025-01-31 08:30:53 +0800

    SUPABASE

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,4 +1,7 @@
 <template>
+    <ul>
+      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
+    </ul>
   <h1>Food</h1>
   <personal-profile/> <br>
   <food-item/> <br>
@@ -7,7 +10,22 @@
   
 </template>
 
-<script></script>
+<script>
+  import { ref, onMounted } from 'vue'
+  import { supabase } from './lib/supabaseClient'
+
+  const countries = ref([])
+
+  async function getCountries() {
+    const { data } = await supabase.from('countries').select()
+    countries.value = data
+  }
+
+  onMounted(() => {
+    getCountries()
+  })
+
+</script>
 
 <style>
 </style>           
---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://clidxtfqlhmfaqmgltkh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNsaWR4dGZxbGhtZmFxbWdsdGtoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNzM2NDMsImV4cCI6MjA1Mzg0OTY0M30.RZAixSu9tawFiYk7UaXFh8VfnNhRN8eJ5qdcB-bswK4')
