07522e3f2e8430c552c4d3284448904299c98648
apcagcatap <agcatap@student.apc.edu.ph>
2025-02-10 00:13:29 +0800

    try

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,21 +1,13 @@
 <template>
+  <div id="app">
     <h1>Comments</h1>
     <ul>
-      <li v-for="comments in comments" :key="comments.id">{{ comments.name }} {{ comments.comment }}</li>
+      <li v-for="comment in comments" :key="comment.id">
+        <strong>{{ comment.name }}:</strong> {{ comment.comment }}
+      </li>
     </ul>
-  </template>
-  
-  <script></script>
-  
-  <style>
-    #app > div {
-      border: dashed black 1px;
-      display: inline-block;
-      margin: 10px;
-      padding: 10px;
-      background-color: lightyellow;
-    }
-  </style>
+  </div>
+</template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
@@ -24,23 +16,34 @@ import { supabase } from '../lib/supabaseClient'
 const comments = ref([])
 
 async function getComments() {
-  const { data } = await supabase.from('comments').select()
-  comments.value = data
+  const { data, error } = await supabase.from('comments').select()
+  if (error) {
+    console.error('Error fetching comments:', error)
+  } else {
+    comments.value = data || []  // Ensure it's an empty array if data is null
+  }
 }
 
 onMounted(() => {
   getComments()
 })
-
 </script>
 
-
 <style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightyellow;
-  }
+#app > div {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightyellow;
+}
+
+ul {
+  padding: 0;
+  list-style: none;
+}
+
+li {
+  margin-bottom: 10px;
+}
 </style>
