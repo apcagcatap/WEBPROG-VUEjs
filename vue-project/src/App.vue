f17ae6d8d85fe790dca62de8e23c02bc7a62c8cd
apcagcatap <agcatap@student.apc.edu.ph>
2025-02-09 22:23:21 +0800

    comments

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-  </ul>
+  <country/>
   <br>
   <comment-form/>
   <comment/>
@@ -14,20 +12,7 @@
   
 </template>
 
-<script setup>
-  import { ref, onMounted } from 'vue'
-  import { supabase } from './lib/supabaseClient'
-
-  const countries = ref([])
-
-  async function getCountries() {
-    const { data } = await supabase.from('countries').select()
-    countries.value = data
-  }
-
-  onMounted(() => {
-    getCountries()
-  })
+  <script>
   </script>
 
 <style>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import FoodItem from './components/FoodItem.vue'
 import FoodItem2 from './components/FoodItem2.vue'
 import PersonalProfile from './components/PersonalProfile.vue'
+import Country from './components/Country.vue'
 import CommentForm from './components/CommentForm.vue'
 import Comment from './components/Comment.vue'
 
@@ -12,6 +13,7 @@ const app = createApp(App)
 app.component('food-item', FoodItem)
 app.component('food-item2', FoodItem2)
 app.component('personal-profile', PersonalProfile)
+app.component('country', Country)
 app.component('comment-form', CommentForm)
 app.component('comment', Comment)
 
