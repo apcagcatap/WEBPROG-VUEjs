9d88d5acccb0b4f8b21c6885a410ca1551c54136
apcagcatap <agcatap@student.apc.edu.ph>
2025-02-11 01:09:47 +0000

    axios

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,8 +4,11 @@
   <comment-form/>
   <comment/>
   <br>
-  <h1>Food</h1>
+  <h1>Rest Api</h1><br>
+  <rest-api/><br>
+  <h1>Personal Profile</h1>
   <personal-profile/> <br>
+  <h1>food</h1>
   <food-item/> <br>
   <food-item2/> <br>
   <food-item/> <br>
